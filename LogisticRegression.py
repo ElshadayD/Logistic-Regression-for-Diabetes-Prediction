671da8d87b419a417d99cafeb53f39146f0697bd
Elshaday_desalegn <71713154+ElshadayD@users.noreply.github.com>
2023-04-03 01:43:10 -0600

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from LogisticRegression import LogisticRegression
 def sigmoid(z):
     return  1 / (1 + np.exp(-z))
 
@@ -31,15 +34,20 @@ def plotGraph(iterations, cost):
     ax.set_ylabel('Cost')
     ax.set_title('Cost vs Iterations')
     plt.show()
-
+def accuracy(y_pred,y_test):
+    return np.sum(y_pred==y_test)/len(y_test)
 def main():
 
     data = pd.read_csv('diabetes.csv')
 
     X = data[['Pregnancies', 'Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']]
     y = data['Outcome']
-
-
+    x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
+    regressor=LogisticRegressionCV()
+    regressor.fit(x_train,y_train)
+    y_pred=regressor.predict(x_test)
+    acc=accuracy(y_pred,y_test)
+    print("the accuracy of the model is ",acc*100,"%")
     X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
 
     w= np.zeros(X.shape[1])
@@ -47,7 +55,7 @@ def main():
     alpha = 0.0001
     iterations = 10000
     pediction = sigmoid(np.dot(X, w)+b)
-     print("Initial cost value for w and b values {0} and {1}is: {2}".format(w, b,compute_cost(prediction,y)))
+    print("Initial cost value for w values {0} is: {1}".format(w, compute_cost(pediction,y)))
 
     w, b,cost_num = gradient_descent(X, y, w,b, alpha, iterations)
 
